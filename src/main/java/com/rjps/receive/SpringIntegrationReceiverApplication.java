509aafbc001177ffd8a1c7c88df38aa577a67349
Raja Jagadeesan <raja_jagadeesan@ymail.com>
2024-04-26 01:46:49 +0530

    Initial commit

---
+++ src/main/java/com/rjps/receive/SpringIntegrationReceiverApplication.java
@@ -0,0 +1,44 @@
+package com.rjps.receive;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
+
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+
+@SpringBootApplication
+public class SpringIntegrationReceiverApplication {
+
+	public static final Logger logger = LogManager.getLogger(SpringIntegrationReceiverApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationReceiverApplication.class, args);
+	}
+
+	@Bean
+	public MessageChannel pubsubInputChannel() {
+		return new DirectChannel();
+	}
+
+	@Bean
+	public PubSubInboundChannelAdapter messageChannelAdapter(
+			@Qualifier("pubsubInputChannel") MessageChannel inputChannel, PubSubTemplate pubSubTemplate) {
+		PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter(pubSubTemplate, "exampleSubscription");
+		adapter.setOutputChannel(inputChannel);
+
+		return adapter;
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "pubsubInputChannel")
+	public void messageReceiver(String payload) {
+		logger.info("Message arrived! Payload: {}", payload);
+	}
+}
---
+++ src/test/java/com/rjps/receive/SpringIntegrationReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjps.receive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
